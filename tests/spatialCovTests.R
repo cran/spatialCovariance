da7b4b47ef0c2c4169bb24477eb584722eff679a
David Clifford <david.clifford+CRAN@gmail.com>
2015-07-05 00:00:00 +0000

    version 0.6-9

--- tests/spatialCovTests.R
+++ tests/spatialCovTests.R
@@ -18,3 +18,22 @@ for(x in xVals)
   }
 
 cat("Passes",sum((yVals-((25-4*pi-4*log(2))/12-log(xVals)))<1e-12),"of 101 tests\n")
+
+nrows <- 2
+ncols <- 2
+rowsep <- 0
+colsep <- 0
+rowwidth <- 1
+colwidth <- 10
+info <- precompute(nrows,ncols,rowwidth,colwidth,rowsep,colsep,cat.level=0)
+V1 <- computeV(info,class="ldt",cat.level=0)
+rowwidth <- 10
+colwidth <- 1
+info <- precompute(nrows,ncols,rowwidth,colwidth,rowsep,colsep,cat.level=0)
+V2 <- computeV(info,class="ldt",cat.level=0)
+cat("Passes", sum(V1 - V2[c(1,3,2,4),c(1,3,2,4)] == 0),"out of 16 additional tests\n")
+
+a <- rowwidth
+b <- colwidth
+(25*a^2*b^2 - 8 * a* b^3 * atan(a/b) - 8* a^3* b* atan(b/a) - 2*a^4*log(a) -2*b^4*log(b) + (a^4 - 6*a^2*b^2 + b^4)*log(a^2 + b^2))/(12*a^2*b^2)
+V2[1,1]
